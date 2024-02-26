eb2946595f11d7568acb859b84ca2abc32c87763
IvanTulin <tulin.v@bk.ru>
2024-02-26 16:46:17 +0300

    Создал шаблонную ветку develop

---
+++ RecipesAppTeamPlate/RecipesAppTeamPlate/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+///  Стартовый экран
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
